ff599cfa2d9de9aa6e78804655fe67efd3d6731e
Ajay Murali <83402481+ajaymurali1998@users.noreply.github.com>
2021-09-05 17:57:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, request, jsonify, render_template, url_for
+import pandas as pd
+import numpy as np
+import pickle
+
+app=Flask(__name__)
+
+pickle_in=open('classifier.pkl','rb')
+classifier=pickle.load(pickle_in)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+
+@app.route('/predict',methods=["POST"])
+    
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
+
+    return render_template('home.html', prediction_text="prediction is {}".format(prediction[0]))
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+    
+
+   
+
+
+
+
